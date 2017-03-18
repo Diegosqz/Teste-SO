aa1761f2770cfa308ffca380393dd430ff2b0766
Sergio Coelho <sergio-coelho20111@hotmail.com>
2017-03-17 21:42:10 -0300

    Update 01

---
+++ Thread_Teste_01_SO/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Thread_Teste_01_SO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Criar thread 0
+            Program.CriarThread(0);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //Criar thread 1
+            Program.CriarThread(1);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            //Criar thread 2
+            Program.CriarThread(2);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Parametros p = new Parametros();
+            p.mensagem = textBox1.Text;
+            p.index = 0;
+            Program.IniciarThread(0, p);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Parametros p = new Parametros();
+            p.mensagem = textBox1.Text;
+            p.index = 1;
+            Program.IniciarThread(1, p);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Parametros p = new Parametros();
+            p.mensagem = textBox1.Text;
+            p.index = 2;
+            Program.IniciarThread(2, p);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Program.PararThread(0);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Program.PararThread(1);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Program.PararThread(2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Program.ExcluirThread(0);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Program.ExcluirThread(1);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Program.ExcluirThread(2);
+        }
+    }
+}
---
+++ Thread_Teste_01_SO/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Thread_Teste_01_SO
+{
+    static class Program
+    {
+        static Thread[] Threads = new Thread[3];
+        static AutoResetEvent[] Events = new AutoResetEvent[3];
+        static Form Form1; 
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            for (int i = 0; i < 3; i++)
+                Events[i] = new AutoResetEvent(false);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        static internal void CriarThread(int index)
+        {
+            //Criar tread no array
+            if (Threads[index] != null)
+            {
+                MessageBox.Show("Processo já foi criado!!!");
+            }
+            Thread thread = new Thread(Trabalhador);
+            Threads[index] = thread;
+        }
+
+        static internal void PararThread(int index)
+        {
+            //Parar thread no array
+            if (Threads[index].ThreadState == ThreadState.Suspended)
+            {
+                MessageBox.Show("Processo Já foi parado!!!");
+                return;
+            }
+
+            Threads[index].Interrupt();
+        }
+
+        static internal void IniciarThread(int index, Parametros p)
+        {
+            //inicia o thread no array
+            if (Threads[index].ThreadState == ThreadState.Running)
+            {
+                MessageBox.Show("Processo Já foi iniciado!!!");
+                return;
+            }
+
+            Threads[index].Start((object) p);
+        }
+
+        static internal void ExcluirThread(int index)
+        {
+            //exclui o thread no array
+            if (Threads[index].ThreadState == ThreadState.Stopped)
+            {
+                MessageBox.Show("Processo Já foi excluido!!!");
+                return;
+            }
+
+            Threads[index] = null;
+        }
+
+        static void Trabalhador(object o)
+        {
+            //laço infinito mostrar messagebox cada dois segundos e encerra em reação ao auto reset event setado
+            Parametros p = (Parametros)o;
+            while (true)
+            {
+                MessageBox.Show("Thread" + p.index, p.mensagem);
+            }
+        }
+    }
+
+    class Parametros
+    {
+        public int index;
+        public string mensagem;
+        public AutoResetEvent evento;
+    }
+}
